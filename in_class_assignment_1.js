1291ee7cee0d82ae73032e2c97cb320a5c4c88f2
lucas0walters <lucas.o.walters@gmail.com>
2025-01-30 16:14:25 -0500

    Practice Problem 1 and 2

---
+++ in_class_assignment_1.js
@@ -0,0 +1,18 @@
+// Task 1 - Practice problem
+
+let expenses = 999999;
+if (expenses > 7000) {
+    console.log("High expenses");
+} else {
+    console.log("Managable expenses");
+};
+
+//Practice Problem 2 - 3 min
+
+let score = 70;
+if (score >= 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+};
+
